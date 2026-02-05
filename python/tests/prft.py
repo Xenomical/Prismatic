9337f5f3171c497462936299be9aaa32557727d2
Xenomical <238035250+Xenomical@users.noreply.github.com>
2026-02-05 11:32:47 -0800

    Auto commit 05-Thu-02 11:32:46

--- python/tests/prft.py
+++ python/tests/prft.py
@@ -1,2 +1,17 @@
-def prft():
-    return 0
+import sys
+from pathlib import Path
+
+SRC = Path(__file__).resolve().parents[1] / "src"
+sys.path.insert(0, str(SRC))
+
+from move import generateMoves
+
+def perft():
+    nodes = len(generateMoves(1|1<<48,(1|1<<48|1<<6|1<<42)))
+    correct = True if nodes==16 else False
+    if correct:
+        print(f"Nodes: {nodes} - Test Succeded")
+    else:
+        print(f"Nodes: {nodes} - Test Failed")
+
+perft()
