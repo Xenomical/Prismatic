3c1a688080adb470a0e7920ae3b60a71c68329a2
Xenomical <238035250+Xenomical@users.noreply.github.com>
2026-02-09 00:32:32 +0100

    updating the perft function
    Ignore that it's called prft.py ;)

--- python/tests/prft.py
+++ python/tests/prft.py
@@ -1,17 +1,32 @@
 import sys
-from pathlib import Path
+import os
 
-SRC = Path(__file__).resolve().parents[1] / "src"
-sys.path.insert(0, str(SRC))
+# determine project root and add src to path
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+src_path = os.path.join(project_root, "src")
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
 
 from move import generateMoves
+from make import makeMove
+from board import Board
 
-def perft():
-    nodes = len(generateMoves(1|1<<48,(1|1<<48|1<<6|1<<42)))
-    correct = True if nodes==16 else False
-    if correct:
-        print(f"Nodes: {nodes} - Test Succeded")
-    else:
-        print(f"Nodes: {nodes} - Test Failed")
+correctValues = [1,16, 256, 6460, 155888, 4752668, 141865520, 5023479496, 176821532236]
 
-perft()
+
+def perft(board, depth):
+	if depth == 0:
+		return 1
+	
+	nodes = 0
+	for move in generateMoves(board.wb if not board.stm else board.bb,board.wb|board.bb|board.blockers):
+		child = board.copyBoard()
+		makeMove(child, move)
+		nodes += perft(child, depth-1)
+		
+	return nodes
+		
+for d in range(4):
+    c = Board()  # starting position
+    nodes = perft(c, d)
+    print(f"Depth {d}: {nodes} - {'OK' if nodes == correctValues[d] else 'WRONG'} - correct Nodes - {correctValues[d]}")
